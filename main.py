1db2ad4e401b794c54aa31b80bce7c4554f4c00a
quantum2681 <quantum.2681@gmail.com>
2023-06-19 02:55:43 +0700

    add test 2 epoch

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ def get_parser():
     parser.add_argument(
         '--save-epoch',
         type=int,
-        default=30,
+        default=1,
         help='the start epoch to save model (#iteration)')
     parser.add_argument(
         '--eval-interval',
