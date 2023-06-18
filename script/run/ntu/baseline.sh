b3e2a36b79f7bdc1f8fde14acac19406da11e8c2
quantum2681 <quantum.2681@gmail.com>
2023-06-19 01:12:03 +0700

    first commit

---
+++ script/run/ntu/baseline.sh
@@ -0,0 +1,5 @@
+python main.py \
+    --config config/nturgbd-cross-view/default.yaml \
+    --work-dir work_dir/ntu_J/cview/ctrgcn \
+    --device 0
+
---
+++ script/run/ucla/baseline.sh
@@ -0,0 +1,4 @@
+python3 main.py \
+    --config config/ucla/default.yaml \
+    --work-dir work_dir/ucla_B/ctrgcn \
+    --device 0
