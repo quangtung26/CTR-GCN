b6e503cdf9912351b5049dbcdc3b77b8ad3b31f2
quangtung26 <quangtung2681@gmail.com>
2023-10-25 14:32:30 +0700

    fix num-worker

--- main_la.py
+++ main_la.py
@@ -135,7 +135,7 @@ def get_parser():
     parser.add_argument(
         '--num-worker',
         type=int,
-        default=8,
+        default=4,
         help='the number of worker for data loader')
     parser.add_argument(
         '--train-feeder-args',
